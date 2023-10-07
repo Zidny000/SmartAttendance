655d14930f8265a45b6929621bcda82f2cba907e
Zidny000 <t.m.zidny@gmail.com>
2023-10-08 00:42:03 +0600

    skeleton re fix

--- Client/src/components/common/sharedLayout/index.js
+++
@@ -1,6 +0,0 @@
-import Footer from './Footer';
-import Greeting from './Greeting';
-import Navbar from './Navbar';
-import PageTitleBreadcrumb from './PageTitleBreadcrumb';
-
-export { Footer, Greeting, Navbar, PageTitleBreadcrumb };
--- Client/src/pages/common/mainMenu/index.js
+++
@@ -1,6 +0,0 @@
-import MainMenu from './MainMenu';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-
-export { MainMenu, SignIn, SignUp };
-
